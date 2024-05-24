517a5ed9a075c80823b18dacf54cfe215a08ce24
HaiDang04 <dangbhhph33065@fpt.edu.vn>
2024-05-24 15:53:50 +0700

    ccc

--- app/src/main/java/com/example/lab3_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lab3_kotlin/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.lab3_kotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab3_kotlin.ui.theme.Lab3_KotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,17 +32,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Lab3_KotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+                GetLayout(title = "Trang Chu")
             }
         }
     }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun GetLayout (title: String = "Trang Chu") {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        getTextTitle(title = count.toString())
+        Button(onClick = {
+            count++
+        }) {
+            Text(text = "Click Me!")
+        }
+    }
+
+}
+
+@Composable
+fun getTextTitle(title: String){
+    Text(
+        text = title,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        fontSize = 30.sp,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -37,7 +90,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab3_KotlinTheme {
